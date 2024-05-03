fecef5f92726c416682dfc1707b9ded5130000a0
Johny <36568058+RaD3v@users.noreply.github.com>
2024-05-03 16:24:15 +0100

    Update _commons.js

--- static/js/_commons.js
+++ static/js/_commons.js
@@ -20367,11 +20367,21 @@
                     (t))
             };
             a("en_US", (function (e, t) {
-                    if (0 === t)
+                    if (0 === t) {
                         return ["току-що", "сега"];
+                    } else {
                     var n = r[Math.floor(t / 2)];
-                    return e > 1 && (n += "а"),
-                    ["преди " + e + " " + n, "in " + e + " " + n]
+                    if (e > 1) {
+                        if (n === 'секунда') n = 'секунди';
+                        else if (n === 'минута') n = 'минути';
+                        else if (n === 'час') n = 'часa';
+                        else if (n === 'ден') n = 'дни';
+                        else if (n === 'седмица') n = 'седмици';
+                        else if (n === 'месец') n = 'месецa';
+                        else if (n === 'година') n = 'години';     
+                    }
+                    return ["преди " + e + " " + n, "след " + e + " " + n];
+                    }
                 })),
             a("zh_CN", (function (e, t) {
                     if (0 === t)
